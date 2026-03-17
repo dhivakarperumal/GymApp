0a2691aba476470fbc96af46717a5294c5cf3c5c
dhanushqtechx003 <qtechx003@gmail.com>
2026-03-17 10:28:39 +0530

    Diet Date change

--- app/(tabs)/diet.js
+++ app/(tabs)/diet.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { getDietPlans } from "../../services/api";
 import { useAuth } from "../../context/AuthContext";
 import { useRouter } from "expo-router";
+import dayjs from "dayjs";
 
 export default function DietChartScreen() {
   const { user } = useAuth();
@@ -11,6 +12,8 @@ export default function DietChartScreen() {
 
   const [diet, setDiet] = useState(null);
   const [title, setTitle] = useState("");
+
+  const [createdAt, setCreatedAt] = useState(null);
 
   const mealTimes = {
     Morning: "06:00 AM",
@@ -31,6 +34,7 @@ export default function DietChartScreen() {
       if (myDiet) {
         setTitle(myDiet.title);
         setDiet(myDiet.days); // store ALL days
+        setCreatedAt(myDiet.created_at);
       }
     } catch (err) {
       console.log("Diet fetch error:", err);
@@ -55,41 +59,50 @@ export default function DietChartScreen() {
       {title && <Text className="text-gray-400 mb-6">{title}</Text>}
 
       {diet &&
-        Object.entries(diet).map(([day, meals]) => (
-          <View
-            key={day}
-            className="bg-[#1c1c1c] rounded-2xl p-5 border border-[#262626] mb-6"
-          >
-            <View className="flex-row items-center justify-between mb-4">
-              <Text className="text-white text-lg font-bold">{day} Meals</Text>
+        Object.entries(diet).map(([day, meals], index) => {
+          const baseDate = dayjs(createdAt);
+          const calculatedDate = baseDate
+            .add(index, "day")
+            .format("DD-MM-YYYY");
 
-              <Ionicons name="restaurant-outline" size={18} color="#ff3c00" />
-            </View>
+          return (
+            <View
+              key={day}
+              className="bg-[#1c1c1c] rounded-2xl p-5 border border-[#262626] mb-6"
+            >
+              <View className="flex-row items-center justify-between mb-4">
+                <Text className="text-white text-lg font-bold">
+                  {calculatedDate} Meals
+                </Text>
 
-            {Object.entries(meals).map(([meal, value]) => (
-              <View
-                key={meal}
-                className="bg-black rounded-xl p-4 mb-3 border border-[#2a2a2a]"
-              >
-                <View className="flex-row items-center justify-between">
-                  <Text className="text-white font-semibold">{meal}</Text>
+                <Ionicons name="restaurant-outline" size={18} color="#ff3c00" />
+              </View>
 
-                  <Text className="text-red-500 text-sm">
-                    {mealTimes[meal]}
+              {Object.entries(meals).map(([meal, value]) => (
+                <View
+                  key={meal}
+                  className="bg-black rounded-xl p-4 mb-3 border border-[#2a2a2a]"
+                >
+                  <View className="flex-row items-center justify-between">
+                    <Text className="text-white font-semibold">{meal}</Text>
+
+                    <Text className="text-red-500 text-sm">
+                      {mealTimes[meal]}
+                    </Text>
+                  </View>
+
+                  <Text className="text-gray-300 text-sm">
+                    {value.food} ({value.quantity})
+                  </Text>
+
+                  <Text className="text-gray-500 text-xs mt-1">
+                    {value.calories} calories
                   </Text>
                 </View>
-
-                <Text className="text-gray-300 text-sm">
-                  {value.food} ({value.quantity})
-                </Text>
-
-                <Text className="text-gray-500 text-xs mt-1">
-                  {value.calories} calories
-                </Text>
-              </View>
-            ))}
-          </View>
-        ))}
+              ))}
+            </View>
+          );
+        })}
 
       {!diet && (
         <View className="items-center mt-16">
